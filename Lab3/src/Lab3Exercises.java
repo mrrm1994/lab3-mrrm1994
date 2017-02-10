02e8417a0dff96a4a0ea240f5a39f4872c3edfab
3010a13 <3010a13@grosella.ece.uprm.edu>
2017-02-10 15:11:52 -0400

    The MyPanelClass was added. The title was changed.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,14 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Michael R.");
+		JFrame myFrame = new JFrame("Michael");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(850, 400);
 		myFrame.setSize(200, 200);
+		
+		  MyPanelClass myPanel = new MyPanelClass();
+          myFrame.getContentPane().add(myPanel);
+		
 		myFrame.setVisible(true);
 	}
 }
